8f1d6fe8086b4a593805eef50b25aec211e6c65e
Student <example@example.com>
2019-10-25 10:14:55 -0700

    add Views for Flavors index and Account INdex

---
+++ Controllers/FlavorsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using SweetFactory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetFactory.Controllers
+{
+    public class FlavorsController : Controller
+    {
+        private readonly SweetFactoryContext _db;
+        
+        
+        public FlavorsController(SweetFactoryContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Flavor> model = _db.Flavors.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Flavor flavor)
+        {
+            _db.Flavors.Add(flavor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        
+    }
+}
