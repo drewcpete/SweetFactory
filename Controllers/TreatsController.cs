d0c1fe125bdf780fbacb1bc7c205ba8fe36ef448
Student <example@example.com>
2019-10-25 13:02:54 -0700

    complete creat treat method.

--- Controllers/TreatsController.cs
+++ Controllers/TreatsController.cs
@@ -33,7 +33,23 @@ namespace SweetFactory.Controllers
 
         public ActionResult Create()
         {
+            ViewBag.FlavorID = new SelectList(_db.Flavors, "FlavorID", "FlavorName");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Treat treat, int FlavorID)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            treat.User = currentUser;
+            _db.Treats.Add(treat);
+            if (FlavorID != 0)
+            {
+                _db.FlavorTreat.Add(new FlavorTreat() { FlavorID = FlavorID, TreatID = treat.TreatID });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
